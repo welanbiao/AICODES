fea96d4be3c2b5b8260f4c1ae4a963ae6aec8133
biao qian <qianbiao2015@outlook.com>
2026-09-08 18:18:46 +0800

    auto: xxr/src/game.ts (2026-09-08 18:18:46)

--- xxr/src/game.ts
+++ xxr/src/game.ts
@@ -613,7 +613,7 @@ export class Game {
     if (dist > 0.0001) {
       const hit = this.scene.pickWithRay(
         new Ray(prev, delta.normalize(), dist + 0.2),
-        (m) => m.isPickable && m.isEnabled() && m.name !== "floor",
+        (m) => m.isPickable && m.isEnabled() && m.name !== "floor" && !isAvatarMesh(m.name),
       );
       if (hit?.hit && hit.pickedMesh && hit.pickedPoint) {
         this.catchPart(hit);
