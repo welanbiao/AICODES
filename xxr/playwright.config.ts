43e5598b9f3b33f6171a973937fe3649a09574d4
biao qian <qianbiao2015@outlook.com>
2026-09-08 19:02:04 +0800

    auto: xxr/playwright.config.ts (2026-09-08 19:02:03)

--- xxr/playwright.config.ts
+++ xxr/playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   },
   webServer: {
     command: "npm run dev",
-    url: "http://127.0.0.1:5180",
+    url: "http://127.0.0.1:5173",
     reuseExistingServer: true,
     timeout: 120_000,
   },
