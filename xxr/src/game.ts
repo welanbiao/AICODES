91ffcd575fc6e8757b0d904f2d505adc2eb8a5d6
biao qian <qianbiao2015@outlook.com>
2026-09-08 16:14:38 +0800

    auto: xxr/src/game.ts (2026-09-08 16:14:37)

--- xxr/src/game.ts
+++ xxr/src/game.ts
@@ -109,6 +109,9 @@ export class Game {
     this.godCam.wheelDeltaPercentage = 0.01;
     this.godCam.minZ = 0.05;
     this.godCam.maxZ = 20000;
+    this.scene.activeCamera = this.godCam;
+
+    this.fpsCam = new UniversalCamera("fps", new Vector3(0, 0, 0), this.scene);
     this.fpsCam.minZ = 0.03;
     this.fpsCam.maxZ = 20000;
     this.fpsCam.inertia = 0;
