b5eb945d48fb5cd47f657c75d05e02ba5999b161
biao qian <qianbiao2015@outlook.com>
2026-09-09 09:40:02 +0800

    auto: xxr/src/avatar.ts (2026-09-09 09:40:02)

--- xxr/src/avatar.ts
+++ xxr/src/avatar.ts
@@ -339,8 +339,8 @@ async function attachBatmanHook(scene: Scene, hook: TransformNode) {
       glbRoot.scaling.scaleInPlace(target / long);
     };
     glbRoot.rotation.x = Math.PI;
-    fitTo(0.07);
-    fitTo(0.07);
+    fitTo(0.028);
+    fitTo(0.028);
     glbRoot.computeWorldMatrix(true);
     const b2 = glbRoot.getHierarchyBoundingVectors(true);
     const center = b2.min.add(b2.max).scale(0.5);
