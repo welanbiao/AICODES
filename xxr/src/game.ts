6a1068f6074a503687cb256f69f0c13f175eeb03
biao qian <qianbiao2015@outlook.com>
2026-09-09 15:19:05 +0800

    auto: xxr/index.html, xxr/src/game.ts (2026-09-09 15:19:05)

--- xxr/src/game.ts
+++ xxr/src/game.ts
@@ -201,7 +201,6 @@ export class Game {
     $<HTMLButtonElement>('[data-testid="btn-explode-fps"]').onclick = () => this.toggleExplode();
     $<HTMLButtonElement>('[data-testid="btn-identify"]').onclick = () => this.identify();
     $<HTMLButtonElement>('[data-testid="btn-fire"]').onclick = () => this.fireHand();
-    $<HTMLButtonElement>('[data-testid="btn-recall"]').onclick = () => this.recallHand();
     $<HTMLButtonElement>('[data-testid="btn-dismount"]').onclick = () => this.dismount();
     $<HTMLButtonElement>('[data-testid="btn-enter"]').onclick = () => this.enterPhone();
   }
