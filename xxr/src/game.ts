adfeca6aeb462a89aca98e8713da84ade4e291c1
biao qian <qianbiao2015@outlook.com>
2026-09-09 10:09:21 +0800

    auto: xxr/src/game.ts (2026-09-09 10:09:19)

--- xxr/src/game.ts
+++ xxr/src/game.ts
@@ -210,7 +210,7 @@ export class Game {
       if (e.code === "KeyF") this.fireHand();
       if (e.code === "KeyR") this.recallHand();
       if (e.code === "KeyX") this.toggleExplode();
-      if (e.code === "KeyG" || e.code === "Enter") this.enterPhone();
+      if (e.code === "KeyG") this.enterPhone();
       if (e.code === "KeyH" || e.code === "Home") this.returnToCenter();
       if (e.code === "Escape") {
         document.exitPointerLock();
