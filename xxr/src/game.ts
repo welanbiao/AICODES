dd3b7c55f9e03a8509105462dc16ba7cc00c48f9
biao qian <qianbiao2015@outlook.com>
2026-09-09 16:59:06 +0800

    auto: xxr/src/game.ts (2026-09-09 16:59:06)

--- xxr/src/game.ts
+++ xxr/src/game.ts
@@ -1308,6 +1308,7 @@ export class Game {
     const longest = Math.max(size.x, size.y, size.z, 0.001);
     this.phoneWrap.scaling.scaleInPlace(INTERIOR_SPAN / longest);
     this.phoneWrap.computeWorldMatrix(true);
+    this.setZoomBase(this.phoneWrap);
     const after = this.scene.getWorldExtends((m) => this.isPhonePart(m) && !!m.getTotalVertices());
     const center = after.min.add(after.max).scale(0.5);
     this.setSideLevelsVisible(false);
