188073edfc225d960afefce279f64f6df16a95a9
xiaozhou <we191440235@163.com>
2026-09-21 23:01:22 +0800

    给总裁项目加上本地预览地址，并修正图片响应类型

--- 总裁/_serve.ps1
+++ 总裁/_serve.ps1
@@ -23,6 +23,10 @@ function Get-ContentType([string]$path) {
     '.css'  { 'text/css; charset=utf-8' }
     '.js'   { 'application/javascript; charset=utf-8' }
     '.json' { 'application/json; charset=utf-8' }
+    '.png'  { 'image/png' }
+    '.jpg'  { 'image/jpeg' }
+    '.jpeg' { 'image/jpeg' }
+    '.webp' { 'image/webp' }
     '.svg'  { 'image/svg+xml' }
     default { 'application/octet-stream' }
   }
